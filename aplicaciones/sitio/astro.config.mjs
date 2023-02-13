dbe02038639a633beb7e91cd80e047154d37def7
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-02-13 16:38:09 -0500

    🔧 optimizar archivos en la versión publicada

--- aplicaciones/sitio/astro.config.mjs
+++ aplicaciones/sitio/astro.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import comprimir from 'astro-compress';
+import bichos from 'astro-critters';
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: './recursos',
   outDir: './publico',
   site: 'https://colev.enflujo.com',
-  integrations: [sitemap()],
+  integrations: [
+    bichos({ path: './publico' }),
+    comprimir({ path: './publico' }),
+
+    sitemap({
+      customPages: ['https://colev.enflujo.com/pronostico-covid19'],
+      i18n: {
+        defaultLocale: 'es',
+        locales: { es: 'es-CO' },
+      },
+    }),
+
+    robotsTxt(),
+  ],
 });
