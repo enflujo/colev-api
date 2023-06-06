d6feb1a0c77133c8c36ca7660bd8bc92e4df5cfb
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-06-06 10:36:21 -0500

    📝 Diferenciar todos de originales

--- aplicaciones/sitio/src/utilidades/lineaTuits/constantes.ts
+++ aplicaciones/sitio/src/utilidades/lineaTuits/constantes.ts
@@ -4,9 +4,9 @@ export const duracion = 250;
 
 export function color(tipo: string, opacidad: number): string {
   const colores: { [color: string]: string } = {
-    todos: `rgba(62, 255, 207, ${opacidad})`, // verde
+    todos: `rgba(62, 255, 255, ${opacidad})`, // azul
     quoted: `rgba(255, 217, 114, ${opacidad})`, // amarillo
-    replied_to: `rgba(137, 209, 248, ${opacidad})`, // azul
+    replied_to: `rgba(240, 209, 208, ${opacidad})`, // azul
     original: `rgba(62, 255, 207, ${opacidad})`, // verde
   };
 
